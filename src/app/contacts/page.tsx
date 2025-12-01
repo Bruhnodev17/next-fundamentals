c46625a9c5d88cbef6152b7e261a75def95dec52
Manoel Bruno <manoelbrunop17@gmail.com>
2025-12-01 15:41:11 -0300

    File System Routing

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,8 @@
+export default function Contacts() {
+  return (
+    <div>
+      {" "}
+      <h1>Página contatos</h1>
+    </div>
+  );
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="flex px-2 py-4 bg-zinc-900 text-white">
+      <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+        <div>NEXT JS</div>
+
+        <nav>
+          <ul className="flex items-center justify-center gap-2">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/dashboard"}>Dashboard</Link>
+            </li>
+            <li>
+              <Link href={"/contacts"}>contacts</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
